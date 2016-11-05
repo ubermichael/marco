564d63729425026b97de1e013dd9e28229d208f5
Michael Joyce <ubermichael@gmail.com>
2016-11-05 11:39:00 -0700

    oops.

--- count.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'vendor/autoload.php';
-
-$iterator = new Nines\Marco\FileIterator($argv[1]);
-while ($record = $iterator->current()) {
-	$iterator->next();
-}
-print $iterator->key();
